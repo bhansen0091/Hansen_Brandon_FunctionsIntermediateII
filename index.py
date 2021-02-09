48b029b477adf8539ff1d230fac85e7801b7d61a
bhansen0091 <bhansen0091@yahoo.com>
2021-02-09 15:33:26 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
+#1--------------------------------------------------------------------------------------------------------
 
 # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 
 x = [[5, 2, 3], [10, 8, 9]]
 
+def change_x_1(param_list):
+    x[1][0] = 15
+    return param_list
 
+print(change_x_1(x))
+#-----------------------------------------------------------------------------------------------------------
 def change_x(param_list, num_to_find, num_to_sub):
     for nested_list in param_list:
         if num_to_find in nested_list:
@@ -23,7 +29,6 @@ students = [
     {'first_name': 'John', 'last_name': 'Rosales'}
 ]
 
-
 def change_name(param_list, to_find, to_sub):
     for param_list_dict in param_list:
         for dict_key in param_list_dict:
@@ -31,7 +36,6 @@ def change_name(param_list, to_find, to_sub):
                 param_list_dict[dict_key] = to_sub
                 return param_list
     return f"{to_find} does not exist"
-
 
 print(change_name(students, "Jordan", "Bryant"))
 print(change_name(students, "Michael", "Bob"))
@@ -43,7 +47,6 @@ sports_directory = {
     'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
     'soccer': ['Messi', 'Ronaldo', 'Rooney']
 }
-
 
 def change_name_3(param_dict, to_find, to_sub):
     for key in param_dict:
@@ -94,3 +97,71 @@ print(change_z(z,20,30))
 print(change_z(z,10,30))
 print(change_z(z,10,1000))
 
+# 2 --------------------------------------------------------------------------------------------------------
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(param_list):
+    for param_list_dict in param_list:
+        temp = ""
+        for key, key_value in param_list_dict.items():
+            if temp != "":
+                print(f"{temp}, {key} - {key_value} ")
+            else:
+                temp = f"{key} - {key_value}"
+
+iterateDictionary(students)
+
+# 3 --------------------------------------------------------------------------------------------------------
+
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, 
+# the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output 
+# Michael
+# John
+# Mark
+# KB
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary2(param_list, to_find):
+    for list_dict in param_list:  # gets the key
+        print(list_dict[to_find])
+
+iterateDictionary2(students, "first_name")
+iterateDictionary2(students, "last_name")
+
+# 4 --------------------------------------------------------------------------------------------------------
+
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each 
+# key along with the size of its list, and then prints the associated values within each key's list. For example:
+# for key, value in param_dict.items():
+#     print(f"{key}, {value}")
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info(param_dict):
+    for key in param_dict:
+        print(f"{len(param_dict[key])} {key.upper()}")
+        for key_value in param_dict[key]:
+            print(key_value)
+
+print_info(dojo)
